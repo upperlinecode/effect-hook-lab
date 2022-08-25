bd066c503ffb5fdf017129ec99da3fd373f8b4f8
Vilian Ivantchev <vilian.ivantchev@giantmachines.com>
2022-08-25 11:05:12 -0400

    Refactor Board component to use a Redux thunk for data fetching.
    Add and implement loading state from redux
    Remove no longer needed component state and reducer.
    
    ref thunks:
    https://redux-toolkit.js.org/usage/usage-guide#async-requests-with-createasyncthunk
    https://redux-toolkit.js.org/usage/usage-with-typescript#createasyncthunk
    
    ref loading state:
    https://redux-toolkit.js.org/api/createAsyncThunk#overview
    
    ref unwrap():
    https://redux-toolkit.js.org/api/createAsyncThunk#handling-thunk-results

--- src/Board/Board.tsx
+++ src/Board/Board.tsx
@@ -1,10 +1,14 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import Category from "../Category/Category";
 import "./Board.css";
-import { Clue, getFiveClues } from "../utils";
+import { Clue } from "../utils";
 
 import { useAppSelector, useAppDispatch } from "../Redux/hooks";
-import { selectAllClues, setAllClues } from "../Redux/cluesSlice";
+import {
+  selectAllClues,
+  fetchCluesByCategory,
+  selectRequestStatus,
+} from "../Redux/cluesSlice";
 
 interface BoardProps {
   categoryNumbers: number[];
@@ -14,44 +18,23 @@ interface BoardProps {
 
 const Board = (props: BoardProps) => {
   const { categoryNumbers, updateScore, reset } = props;
-  const [isLoading, setIsLoading] = useState<boolean>(false);
+  // const [isLoading, setIsLoading] = useState<boolean>(false);
 
   const dispatch = useAppDispatch();
 
   // selector: select data from Redux state
   const allClues: Clue[][] = useAppSelector((state) => selectAllClues(state));
+  const loadingState = useAppSelector((state) => selectRequestStatus(state));
 
   useEffect(() => {
     const getQuestions = async (categoryNumbers: number[]) => {
-      setIsLoading(true);
-
-      // SO ref on Promise.all with fetch
-      // https://stackoverflow.com/questions/31710768/how-can-i-fetch-an-array-of-urls-with-promise-all
-
-      // MDN Promise.all
-      // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise/all
-
-      // Basically we should use Promise.all if we are using an array of Promises
-      // and need to await for all of them to resolve.
-      //
-      // Promise.all keeps the passed in array order regardless of when each
-      // promise resolves.
-
-      const responses = categoryNumbers.map(async (catId) => {
-        const response = await fetch(
-          `https://jservice.io/api/clues?category=${catId}`
-        );
-
-        const newClues: Clue[] = await response.json();
-        const topFiveClues = getFiveClues(newClues);
-        return topFiveClues;
-      });
-
-      const allClues = await Promise.all(responses);
-
-      // dispatch the action/reducer
-      dispatch(setAllClues(allClues));
-      setIsLoading(false);
+      // ref unwrap():
+      // https://redux-toolkit.js.org/api/createAsyncThunk#handling-thunk-results
+      try {
+        await dispatch(fetchCluesByCategory(categoryNumbers)).unwrap();
+      } catch (error) {
+        console.error("thunk error: ", error);
+      }
     };
 
     getQuestions(categoryNumbers);
@@ -60,7 +43,7 @@ const Board = (props: BoardProps) => {
   //Hover your cursor over the catNum variable to see how Typescript infers its type
   return (
     <>
-      {isLoading && <div>Loading...</div>}
+      {loadingState === "pending" && <div>Loading...</div>}
       <div className="Board">
         {allClues?.map((clues: Clue[], i: number) => {
           return (
--- src/Redux/cluesSlice.ts
+++ src/Redux/cluesSlice.ts
@@ -1,22 +1,78 @@
-import { createSlice } from '@reduxjs/toolkit'
-import type { Clue } from '../utils';
+/*
+ref thunks:
+https://redux-toolkit.js.org/usage/usage-guide#async-requests-with-createasyncthunk
+https://redux-toolkit.js.org/usage/usage-with-typescript#createasyncthunk
+
+ref loading state:
+https://redux-toolkit.js.org/api/createAsyncThunk#overview
+*/
+
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
+import { Clue, getFiveClues } from '../utils';
 import type { RootState } from './store';
 
 export interface SliceState {
-  byCategory: Clue[][]
+  byCategory: Clue[][];
+  // Per documentation. We use only idle and pending.
+  loading: 'idle' | 'pending' | 'succeeded' | 'failed'
 }
 
 const initialState: SliceState = {
-  byCategory: []
+  byCategory: [],
+  loading: 'idle'
 }
+
+// Thunks
+export const fetchCluesByCategory = createAsyncThunk(
+  'questions/fetchCluesByCategory',
+  async (categoryNumbers: number[]) => {
+    // SO ref on Promise.all with fetch
+    // https://stackoverflow.com/questions/31710768/how-can-i-fetch-an-array-of-urls-with-promise-all
+
+    // MDN Promise.all
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise/all
+
+    // Basically we should use Promise.all if we are using an array of Promises
+    // and need to await for all of them to resolve.
+    //
+    // Promise.all keeps the passed in array order regardless of when each
+    // promise resolves.
+
+    const responses = categoryNumbers.map(async (catId) => {
+      const response = await fetch(
+        `https://jservice.io/api/clues?category=${catId}`
+      );
+
+      const newClues: Clue[] = await response.json();
+      const topFiveClues = getFiveClues(newClues);
+      return topFiveClues;
+    });
+
+    return await Promise.all(responses);
+  }
+)
+
 
 export const cluesSlice = createSlice({
   name: 'clues',
   initialState,
-  reducers: {
-    setAllClues: (state, action) => {
-      state.byCategory  = action.payload
-    }
+  reducers: {},
+  // This is the big benefit of using the createAsyncThunk api. Another benefit
+  // is viewing the actions history in the redux dev tools. The extraReducers
+  // api is also used when this slice has to respond to another slice's actions.
+  extraReducers: (builder) => {
+    builder
+      // for our use case (and many others), we only need pending and idle loading states
+      .addCase(fetchCluesByCategory.pending, (state) => {
+        state.loading = 'pending'
+      })
+      .addCase(fetchCluesByCategory.fulfilled, (state, action) => {
+        state.byCategory = action.payload
+        state.loading = 'idle'
+      })
+      .addCase(fetchCluesByCategory.rejected, (state) => {
+        state.loading = 'idle'
+      })
   }
 })
 
@@ -24,6 +80,6 @@ export const selectAllClues = (state: RootState) => (
   state.clues.byCategory
 )
 
-export const { setAllClues } = cluesSlice.actions
+export const selectRequestStatus = (state: RootState) => state.clues.loading
 
 export default cluesSlice.reducer
